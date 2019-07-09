85acfc97ee37ca552ba63af961e79ac228638ec1
Rituraj <riturajguwahati26@gmail.com>
2019-07-09 08:57:38 +0530

    removed about page

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,6 @@ router.get('/', (req, res) => {
 	res.render('home');
 });
 
-router.get('/about', (req, res) => {
-	res.render('about');
-});
-
 router.get('/contact', (req, res) => {
 	res.render('contact');
 });
