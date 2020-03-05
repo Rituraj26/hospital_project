1a3cce980fc7827ef43b1ce4aa4416421407419c
Rituraj <riturajguwahati26@gmail.com>
2020-03-05 11:26:30 +0530

    added mongo atlas config

--- config/database.js
+++ config/database.js
@@ -1,5 +1,8 @@
-if(process.env.NODE_ENV === 'production') {
-	module.exports = {mongoURI: 'mongodb://'}
+if (process.env.NODE_ENV === "production") {
+    module.exports = {
+        mongoURI:
+            "mongodb+srv://Rituraj:mongoAtlasPassword265@cluster0-8izec.mongodb.net/test?retryWrites=true&w=majority"
+    };
 } else {
-	module.exports = {mongoURI: 'mongodb://localhost/medical'}
+    module.exports = { mongoURI: "mongodb://localhost/medical" };
 }
